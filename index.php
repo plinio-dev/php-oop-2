a2292abf57bf0b6b0cb9527ed8a4d9258d5880ed
Tommaso P <tommaso.plinio@gmail.com>
2021-05-12 17:12:29 +0200

    parent

--- index.php
+++ index.php
@@ -19,8 +19,9 @@ class Smartphone extends Prodotto {
    public $marca;
    public $colore;
 
-   public function __construct(string $_marca, string $_colore)
+   public function __construct(string $_nome, int $_prezzo, string $_marca, string $_colore)
 	{
+      parent::__construct($_nome, $_prezzo);
 		$this->marca = $_marca;
 		$this->colore = $_colore;
 	}
@@ -32,11 +33,33 @@ class Lavatrice extends Prodotto {
    public $caricoMax;
    public $giri;
 
-   public function __construct(string $_caricoMax, string $_giri, )
+   public function __construct(string $_nome, int $_prezzo, string $_caricoMax, string $_giri, )
 	{
+      parent::__construct($_nome, $_prezzo);
 		$this->caricoMax = $_caricoMax;
 		$this->giri = $_giri;
 	}
 
 }
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Php oop 2</title>
+</head>
+<body>
+   <h1>Prodotti</h1>
+</body>
+</html>
+
+
+
 
